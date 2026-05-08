7fcc7b3a8aff98ad00b4b9213354229fbee95b1d
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-05-08 05:15:26 -0500

    chore(backend): initialize Spring Boot project structure

---
+++ backend/src/test/java/ec/tecazuay/medista/MedistaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.tecazuay.medista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
