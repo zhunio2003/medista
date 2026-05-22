0bbc8fa4db0eb73a675b8cb220e02228be8a9b64
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-05-22 11:50:55 -0500

    feat(backend/security): add RefreshTokenResponse DTO

---
+++ backend/src/main/java/ec/insti/medista/security/api/dto/RefreshTokenResponse.java
@@ -0,0 +1,13 @@
+package ec.insti.medista.security.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RefreshTokenResponse {
+
+    private String refreshToken;
+    private String accessToken;
+
+}
